5ab9419364c4f11335d9f8ff61c553b4d2840098
Kartikk-git <koundal.k29@gmail.com>
2026-02-06 23:41:48 +0530

    day 2

---
+++ src/arithmeticOperators.kt
@@ -0,0 +1,16 @@
+fun main(){
+    var a = 10
+    var b = 20
+
+    var c = a + b
+    println("The result is $c")
+
+    a += 10
+    var d = 30
+
+    println("The result is ${a + d}")
+
+    val e = 30
+    println("This value cannot be changed $e")
+
+}
