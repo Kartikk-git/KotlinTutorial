fb580da609427b2caa455c71bd2a4d4290a610e3
Kartikk-git <koundal.k29@gmail.com>
2026-02-06 23:41:48 +0530

    day 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,29 @@
 fun main() {
-    val name = "Kotlin"
 
-    println("Hello, " + name + "!")
+    /*
+    println("Hello World!!!")
 
-    for (i in 1..5) {
-        println("i = $i")
-    }
+    var myVariable: Int = 10
+    println(myVariable)
+
+    var myVariable1 = 10
+    println(myVariable1)
+
+
+    var myVariable2 = 20
+    println("Value of myVariable2 is $myVariable2")
+    myVariable2 = 50
+    println("Now the value is $myVariable2")
+
+
+    val myVariable3 = 30
+    println("Value of variable 3 is $myVariable3")
+    myVariable3 = 10
+    println("Value of variable 3 is $myVariable3")
+*/
+
+    var myVariable : Boolean = true
+    println("value of myVariable = $myVariable")
+
+
 }
---
+++ src/arithmeticOperators.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var a = 10
+    var b = 20
+
+    var c = a + b
+    println("The result is $c")
+
+    a += 10
+    var d = 30
+
+    println("The result is ${a + d}")
+}
