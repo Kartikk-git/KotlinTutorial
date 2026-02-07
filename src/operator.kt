b6fbf94584b43fd548e51f7abc10a33cb99ecc96
Kartikk-git <koundal.k29@gmail.com>
2026-02-08 02:02:54 +0530

    Added arithmetic, logical operators (#7)

---
+++ src/operator.kt
@@ -0,0 +1,20 @@
+fun main()
+{
+    // Arithmetic Operators = { + , _ , / , * }
+    var a = 10
+    val b = 20
+    val c = a + b
+    println("The result is $c")
+
+    a += 10
+    val d = 30
+    println("The result is ${a + d}")
+
+    // Comparison Operator = { == , != , < , > , >= , <= }
+    println( a == b )
+    println ( a > b )
+
+
+
+
+}
